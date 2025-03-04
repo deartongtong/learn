361687c44125f136d0241ce91230d0fc42466797
lihaotong <lihaotong@lihaotongdeMac-mini.local>
2025-03-04 13:48:00 +0800

    Initial commit: firest add Xcode project files

---
+++ learn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  learn
+//
+//  Created by 李昊彤 on 2025/3/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ learnTests/learnTests.swift
@@ -0,0 +1,17 @@
+//
+//  learnTests.swift
+//  learnTests
+//
+//  Created by 李昊彤 on 2025/3/4.
+//
+
+import Testing
+@testable import learn
+
+struct learnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
